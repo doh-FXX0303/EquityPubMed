c3e6af329195843147c56c793f8dce929ba0ddda
Fan Xiong <fan.xiong@doh.wa.gov>
2023-11-18 02:02:37 -0800

    Update 1-EquityPubMed-Topic-Modeling.R

--- R/1-EquityPubMed-Topic-Modeling.R
+++ R/1-EquityPubMed-Topic-Modeling.R
@@ -10,50 +10,39 @@ library(doParallel)     # For parallel computing
 library(pubmedR)        # For accessing PubMed data
 
 # Set the API key for PubMed access
+  # You may not need one but it could be slower.
 api_key <- ""
 
-# Define a range of years and a search term
-year <- 1944:2023   # Define the year range
+# Define a range of pub_years and a search term
+pub_year <- 1944:2023   # Define the pub_year range
 term <- "Asian"     # Define the search term
 
 # Create directories for storing search results
 lapply(paste0("data/Search-", term), dir.create)           # Create main directory for each search term
-lapply(paste0("data/Search-", term, "/", year), dir.create) # Create subdirectories for each year
+lapply(paste0("data/Search-", term, "/", pub_year), dir.create) # Create sub directories for each pub_year
 
-# Generate combinations of years and the search term formatted for PubMed query syntax
+# Generate combinations of pub_years and the search term formatted for PubMed query syntax
 term_grid <-
   expand.grid(
-    "year" =  sprintf(
-      '(("%d/01/01"[Date - Publication] : "%d/12/31"[Date - Publication]))', year, year
+    "pub_year" =  sprintf(
+      '(("%d/01/01"[Date - Publication] : "%d/12/31"[Date - Publication]))', pub_year, pub_year
     ),
-    "term" = term |> paste0('[Title])')
+    "term" = term |> paste0('[All])')
   ) %>%
-  dplyr::bind_cols(data.frame(year_search = year)) %>%
-  dplyr::bind_cols(data.frame(term_search = term))
-
-# Write the query combinations to CSV files
-term_grid %>%
-  dplyr::group_by(term_search, year_search) %>%
-  dplyr::group_walk( ~ readr::write_csv(
-    .x,
-    path = paste0("data/Search-", term, "/", .y$year_search, "/search_", .y$term_search, '.csv'),
-    num_threads = parallel::detectCores()
-  ))
-
-# List the files containing search queries
-search_files <-
-  list.files(
-    paste0("data/Search-", term),
-    full.names = TRUE,
-    recursive = TRUE,
-    pattern = "search_"
+  dplyr::bind_cols(data.frame(pub_year_search = pub_year)) %>%
+  dplyr::bind_cols(data.frame(term_search = term)) %>%
+  dplyr::mutate(query =  paste0(
+    pub_year,
+    ' AND ',
+    paste0('(', term, ')')
+  )
   )
 
 # Create directories for batch and article data
 lapply(paste0("data/Batch-", term), dir.create)
-lapply(paste0("data/Batch-", term, "/", year), dir.create)
+lapply(paste0("data/Batch-", term, "/", pub_year), dir.create)
 lapply(paste0("data/Article-", term), dir.create)
-lapply(paste0("data/Article-", term, "/", year), dir.create)
+lapply(paste0("data/Article-", term, "/", pub_year), dir.create)
 
 # Download and process PubMed data
 foreach::foreach(
@@ -67,7 +56,7 @@ foreach::foreach(
   # Construct the data query
   data_query <-
     data.frame(final =  paste0(
-      search_term_df$year,
+      search_term_df$pub_year,
       ' AND ',
       paste0('(', search_term_df$term, ')')
     ))
@@ -75,8 +64,8 @@ foreach::foreach(
   # Download articles from PubMed
   out <-
     easyPubMed::batch_pubmed_download(
-      data_query$final,
-      dest_dir =  paste0("data/Batch-", term, "/", year[i]),
+      term_grid$query[i],
+      dest_dir =  paste0("data/Batch-", term, "/", pub_year[i]),
       dest_file_prefix = "articles_",
       format = "xml",
       api_key = api_key,
@@ -87,17 +76,18 @@ foreach::foreach(
   out2 <- foreach::foreach(j = 1:length(out)) %do% {
     # Extract and format article data
     out2 <- easyPubMed::table_articles_byAuth(
-      paste0("data/Batch-", term, "/", year[i], "/", out[j]),
+      paste0("data/Batch-", term, "/", pub_year[i], "/", out[j]),
       included_authors = "first",
       max_chars = 10000,
       encoding = "ASCII"
     ) %>%
       dplyr::mutate(across(c("year", "month", "day"), as.integer)) %>%
-      dplyr::mutate(date = lubridate::make_date(year = year, month = month, day = day)) %>%
-      dplyr::mutate(query = data_query$final)
+      dplyr::mutate(query = term_grid$query[i]) %>%
+      dplyr::mutate(pub_year = pub_year[i])
+    
     
     # Write the processed data to CSV files
-    readr::write_csv(out2, file = paste0("data/Article-", term, "/", year[i], "/pubmed_", j, ".csv"), num_threads = 4)
+    readr::write_csv(out2, file = paste0("data/Article-", term, "/", pub_year[i], "/pubmed_", j, ".csv"), num_threads = 4)
   }
   
   # Clean up memory by removing temporary variables
